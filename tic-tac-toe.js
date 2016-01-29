62ae65066e77de1d4c01a5208e0e4f3a06b3088e
Daphne <dg1181@nyu.edu>
2016-01-28 21:54:51 -0800

    Winning game is working

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,25 +1,58 @@
+Array.prototype.allSame = function() {
+  for(var i = 1; i < this.length; i++) {
+    if (this[0] !== this[i] || this[i] === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
 function TicTacToe() {
-  this.board = [[], [], []];
+  this.moves = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
   this.turn = 0;
-  this.rowOne = this.board[0];
-  this.rowTwo = this.board[1];
-  this.rowThree = this.board[2];
 }
 
-var boxes = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+// var boxes = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
 TicTacToe.prototype = {
+  winner: function(r, c, player) {
+    var row = this.moves[r];
+    var col = [this.moves[0][c], this.moves[1][c], this.moves[2][c]];
+
+    // check row & column
+    if (row.allSame() || col.allSame()) {
+      $("#winner").text(player + " wins!");
+    }
+
+    if ((r + c) % 2 === 0) {
+      var diagOne = [this.moves[0][0], this.moves[1][1], this.moves[2][2]];
+      var diagTwo = [this.moves[2][0], this.moves[1][1], this.moves[0][2]];
+
+      // check diagonals
+      if (diagOne.allSame() || diagTwo.allSame()) {
+        $("#winner").text(player + " wins!");
+      }
+    }
+  },
+
   play: function() {
     var self = this;
-    boxes.forEach(function(box) {
-      $("#" + box).click(function() {
-        if (self.turn % 2 === 0) {
-          $(this).append('<img src="http://www.clker.com/cliparts/0/7/e/a/12074327311562940906milker_X_icon.svg.hi.png">');
-        } else {
-          $(this).append('<img src="http://www.clipartbest.com/cliparts/9c4/bn9/9c4bn9Adi.png">');
-        }
-        self.turn++;
-      });
+
+    $("td").one("click", function() {
+      var box = this.id;
+      var r = parseInt(box.match(/\d+/g)[0]), c = parseInt(box.match(/\d+/g)[1]);
+
+      if (self.turn % 2 === 0) {
+        $("#" + box).text("X");
+        self.moves[r][c] = "X";
+        self.winner(r, c, "X");
+      } else {
+        $("#" + box).text("O");
+        self.moves[r][c] = "O";
+        self.winner(r, c, "O");
+      }
+
+      self.turn++;
     });
   }
 };
