27ecf6bc83976a2bce208a8663f07bf852ae9bdf
Daphne <dg1181@nyu.edu>
2016-01-29 10:54:52 -0800

    Working game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -12,8 +12,6 @@ function TicTacToe() {
   this.turn = 0;
 }
 
-// var boxes = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-
 TicTacToe.prototype = {
   winner: function(r, c, player) {
     var row = this.board[r];
@@ -21,22 +19,52 @@ TicTacToe.prototype = {
 
     // check row & column
     if (row.allSame() || col.allSame()) {
-      $("#winner").text(player + " wins!");
-    }
-
-    if ((r + c) % 2 === 0) {
+      $("#overlay").text(player + " Wins! Play again?");
+      $("#overlay").addClass("winner");
+      this.again();
+    } else if ((r + c) % 2 === 0 && this.turn !== 8) {
       var diagOne = [this.board[0][0], this.board[1][1], this.board[2][2]];
       var diagTwo = [this.board[2][0], this.board[1][1], this.board[0][2]];
 
       // check diagonals
       if (diagOne.allSame() || diagTwo.allSame()) {
-        $("#winner").text(player + " wins!");
+        $("#overlay").text(player + " Wins! Play again?");
+        $("#overlay").addClass("winner");
+        this.again();
       }
-    }
+    } else if (this.turn === 8) {
+       $("#overlay").text("Tie game! Play again?");
+       $("#overlay").addClass("winner");
+       this.again();
+     }
+  },
+
+  again: function() {
+    var self = this;
+    $("table").addClass("again");
+
+    $("#overlay").click(function() {
+      $("table").removeClass("again");
+      $("#overlay").removeClass("winner");
+      $("#overlay").empty();
+
+      for (r = 0; r < 3; r++) {
+        for (c = 0; c < 3; c++) {
+          $("#r" + r + "c" + c).empty();
+          $("#r" + r + "c" + c).removeClass();
+        }
+      }
+
+      self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+      self.turn = 0;
+      self.play();
+    });
   },
 
   play: function() {
     var self = this;
+
+    $("td").off("mouseenter");
     $("td").mouseenter(function () {
       var box = this.id;
       if (!$("#" + box).hasClass("played")) {
@@ -48,6 +76,7 @@ TicTacToe.prototype = {
       }
     });
 
+    $("td").off("mouseleave");
     $("td").mouseleave(function () {
       var box = this.id;
       if (!$("#" + box).hasClass("played")) {
@@ -55,7 +84,7 @@ TicTacToe.prototype = {
       }
     });
 
-
+    $("td").off("click");
     $("td").one("click", function() {
       var box = this.id;
       var r = parseInt(box.match(/\d+/g)[0]), c = parseInt(box.match(/\d+/g)[1]);
@@ -63,13 +92,11 @@ TicTacToe.prototype = {
       if (self.turn % 2 === 0) {
         $("#" + box).text("X");
 
-        self.board[r][c] = "X";
-        self.winner(r, c, "X");
+        self.board[r][c] = "X"; self.winner(r, c, "X");
       } else {
         $("#" + box).text("O");
 
-        self.board[r][c] = "O";
-        self.winner(r, c, "O");
+        self.board[r][c] = "O"; self.winner(r, c, "O");
       }
 
       $("#" + box).addClass("played");
